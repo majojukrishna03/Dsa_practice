f8a10bb3f2efad05e09e2dcc1c1c54a5cc419127
majojukrishna03 <majojukrishna03@gmail.com>
2024-08-11 11:12:52 +0530

    pattern 13

--- patterns.py
+++ patterns.py
@@ -208,3 +208,23 @@ pattern12(5)
 print()
 print("----------------------------------")
 
+# Pattern 13
+'''
+1
+2 3
+4 5 6
+'''
+def pattern13(n):
+    print("Pattern 13")
+    a=1
+    for i in range(1,n+1):
+        for j in range(i):
+            print(a,end=" ")
+            a+=1
+        print()
+
+pattern13(5)
+print()
+print("----------------------------------")
+
+
