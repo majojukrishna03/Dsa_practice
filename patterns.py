2ad2e83444205d0c40eb9c4b4d07dac6d651f117
majojukrishna03 <majojukrishna03@gmail.com>
2024-08-11 10:24:55 +0530

    pattern 10

--- patterns.py
+++ patterns.py
@@ -151,5 +151,26 @@ pattern9(5)
 print()
 print("----------------------------------")
 
+# Pattern 10
+'''
+*
+**
+***
+**
+*
+'''
+def pattern10(n):
+    print("Pattern 10")
+    for i in range(1,n):
+        print("*"*i)
+    for i in range(n,0,-1):
+        print("*"*i)
+
+pattern10(5)
+print()
+print("----------------------------------")
+
+
+
 
 
