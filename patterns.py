718f46fc3371ee8779b21b9866c4429e96593b3a
majojukrishna03 <majojukrishna03@gmail.com>
2024-08-11 11:11:49 +0530

    pattern 12

--- patterns.py
+++ patterns.py
@@ -187,3 +187,24 @@ pattern11(5)
 print()
 print("----------------------------------")
 
+# Pattern 12
+'''
+1         1
+1 2     2 1
+1 2 3 3 2 1
+'''
+def pattern12(n):
+    print("Pattern 12")
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print(j,end=" ")
+        for j in range((n-i)*2):
+            print(" ",end=" ")
+        for j in range(i,0,-1):
+            print(j,end=" ")
+        print()
+
+pattern12(5)
+print()
+print("----------------------------------")
+
